1e1f8ba16a39825e9697ece2300582181204a1de
szkristof32 <kristofreg@gmail.com>
2026-03-06 07:27:38 +0100

    Public interface for clients

---
+++ UniverseEngine/src/UniverseEngine.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "pch.h"
+
+#include "UniverseEngine/Core/Base.h"
+#include "UniverseEngine/Core/Reference.h"
+#include "UniverseEngine/Core/Buffer.h"
+
+#include "UniverseEngine/Core/Events/Event.h"
+#include "UniverseEngine/Core/Events/ApplicationEvents.h"
+#include "UniverseEngine/Core/Events/KeyboardEvents.h"
+#include "UniverseEngine/Core/Events/MouseEvents.h"
+#include "UniverseEngine/Core/Events/DebugEvents.h"
+
+#include "UniverseEngine/Renderer/Buffers.h"
+#include "UniverseEngine/Renderer/VertexArray.h"
+#include "UniverseEngine/Renderer/Shader.h"
+#include "UniverseEngine/Renderer/RendererAPI.h"
